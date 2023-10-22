f13b044ffa58b96a7ac38b2d35a1b887fd6368f2
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-10-22 12:36:20 -0500

    added Navbar component file and removed tailwindcss

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div>
         <div>
             <h4 >Software Developer</h4>
-            <h1 className='text-3xl font-bold underline text-center'>Vincent Aldiano</h1>
+            <h1>Vincent Aldiano</h1>
             {/* <div class="social">
                 <a href="#"><i class='bx bxl-javascript' ></i></a>
                 <a href="#"><i class='bx bxl-html5' ></i></a>
@@ -14,21 +14,17 @@ const Home = () => {
                 <a href="#"><i class='bx bxl-css3' ></i></a>
                 
             </div> */}
-            <p className='text-slate-500 text-center'>I have knowledge in HTML, CSS, JavaScript, Ruby and Rails!</p>
+            <img 
+                src="IMG_1911.JPG" 
+                alt="resume_image" 
+            />
+            <p>I have knowledge in HTML, CSS, JavaScript, Ruby and Rails!</p>
             {/* <div class="main_btn">
                 <a href="#" class="btn">Download CV</a>
                 <a href="#" class="btn two">Hire Me Now</a>
 
             </div> */}
         </div>
-        <div>
-            {/* <img src="IMG_1911.JPG" alt="" /> */}
-            <img 
-                src="IMG_1911.JPG" 
-                alt="resume_image" 
-            />
-        </div>
-
         <ProjectPage />
     </div>
   )
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>ViNcEnT</h1>        
+        <div>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+        </div>
+        {/* <div class="menu_btn">
+            <span>INTRO</span>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div> */}
+    </div>
+  )
+}
+
+export default Navbar
