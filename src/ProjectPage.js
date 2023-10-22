f31b7be0da189c871f9434d41355a25111f9dbd4
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-10-22 09:22:42 -0500

    added ProjectPage component

---
+++ src/ProjectPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <div>
+        
+        <div class="projects_container">
+            
+            {/* <div class="project">
+                <a href="https://clone-273b4.web.app">Amazon Clone App Project</a>
+                <a href="https://github.com/Bentot23/amazon-clone.git"><i class='bx bx-code' ></i></a>
+            </div>
+            <div class="project">
+                <a href="https://netflix-app-clone-project.web.app">Netflix Clone App Project</a>
+                <a href="https://github.com/Bentot23/netflix-app-clone-project.git"><i class='bx bx-code' ></i></a>
+            </div> */}
+        </div>
+
+        <div class="share">
+            <p>Check out my:</p>
+            <div class="social">
+                {/* <a href="https://github.com/Bentot23"><i class='bx bxl-github' undefined ></i></a>
+                <a href="https://linkedin.com/in/vincent-aldiano-789871171"><i class='bx bxl-linkedin' ></i></a>
+                <a href="#"><i class='bx bxl-messenger' ></i></a> */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectPage
