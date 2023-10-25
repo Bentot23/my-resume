a01ac254d2eca476dffeda5020a5035e48f7c8b0
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-10-25 11:17:09 -0500

    updated Navbar links functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProjectPage from './ProjectPage';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/projects'
+            element={ <ProjectPage /> } />
+          <Route path='/'
+            element={ <Home /> } /> 
+        </Routes>
+      </Router>
+    </ThemeProvider>
+    
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,53 @@
 import React from 'react'
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+// import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
+
+// function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+//     event.preventDefault();
+//     console.info('You clicked a breadcrumb.');
+// }
+function handleClick(e) {
+    e.preventDefault()
+    console.log('You clicked Navbar button')
+}
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>ViNcEnT</h1>        
-        <div>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-        </div>
-        {/* <div class="menu_btn">
-            <span>INTRO</span>
-            <div class="bx bx-menu" id="menu-icon"></div>
-        </div> */}
+
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" to="/about">
+          About
+        </Link>
+        <Link   
+          underline="hover"
+          color="inherit"
+          to="/projects">
+          Projects
+        </Link>
+        <Link
+          underline="hover"
+          color="text.primary"
+        //   href="/material-ui/react-breadcrumbs/"
+          to="/"
+          aria-current="page">
+          Home
+        </Link>
+      </Breadcrumbs>
     </div>
+    // <div>
+    //     <h1>ViNcEnT</h1>        
+    //     <div>
+    //         <li>Home</li>
+    //         <li>About</li>
+    //         <li>Projects</li>
+    //     </div>
+    //     {/* <div class="menu_btn">
+    //         <span>INTRO</span>
+    //         <div class="bx bx-menu" id="menu-icon"></div>
+    //     </div> */}
+    // </div>
   )
 }
 
