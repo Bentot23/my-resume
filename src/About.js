82f7909a35cd3ec39298e097d2cec99d967fb770
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2025-03-02 22:17:47 -0600

    updated some texts in About and ProjectPage

--- src/About.js
+++ src/About.js
@@ -6,65 +6,66 @@ import image from "./images/MyPic.png";
 import Socials from "./Socials";
 import Footer from "./Footer";
 
-
-
 const About = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	// const currentSEO = SEO.find((item) => item.page === "about");
+  // const currentSEO = SEO.find((item) => item.page === "about");
 
-	return (
-		<>
-			<div className="page_content">
-				<Navbar active="about" />
-				<div className="content_wrapper">
-					{/* <div className="about-logo-container">
+  return (
+    <>
+      <div className="page_content">
+        <Navbar active="about" />
+        <div className="content_wrapper">
+          {/* <div className="about-logo-container">
 						<div className="about-logo">
 							<Logo width={46} />
 						</div>
 					</div> */}
 
-					<div className="about_container">
-						<div className="about_main">
-							<div className="about_right_side">
-								<div className="title about_title">
-									I'm Vincent A. I live in Texas where I started to learn and love coding!
-								</div>
+          <div className="about_container">
+            <div className="about_main">
+              <div className="about_right_side">
+                <div className="title about_title">
+                  I'm Vincent A. I live in Texas where I started to learn and
+                  love coding!
+                </div>
 
-								<div className="subtitle about_sub">
-									I studied Software Engineering in a bootcamp in Flatiron School. I learned HTML, CSS, Vanilla JavaScript, ReactJS, Ruby, Ruby on Rails, SQL, Postgresql and different UI CSS frameworks. I continue to hone my skills through self education, creating new projects and learning new languages. Right now, I am learning Python and Java, which I am very excited about.
-								</div>
-							</div>
+                <div className="subtitle about_sub">
+                  I studied software development and learned coding languages
+                  such as HTML, CSS, Vanilla JavaScript, ReactJS, Ruby on Rails,
+                  SQL, and different UI CSS frameworks. I continue to hone my
+                  skills through self education, creating new projects, and
+                  learning new languages. I am learning Python, which I am very
+                  excited about right now. I am also learning Wordpess to
+                  develop my web development skills.
+                </div>
+              </div>
 
-							<div className="about_left_side">
-								<div className="about_image_container">
-									<div className="about_image_wrapper">
-										<img
-											src={image}
-											alt=""
-											className="about_image"
-										/>
-									</div>
-								</div>
+              <div className="about_left_side">
+                <div className="about_image_container">
+                  <div className="about_image_wrapper">
+                    <img src={image} alt="" className="about_image" />
+                  </div>
+                </div>
 
-								<div className="about_socials">
-									<Socials />
-								</div>
-							</div>
-						</div>
-						<div className="about_socials_mobile">
-							<Socials />
-						</div>
-					</div>
-					<div className="page_footer">
-						<Footer />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                <div className="about_socials">
+                  <Socials />
+                </div>
+              </div>
+            </div>
+            <div className="about_socials_mobile">
+              <Socials />
+            </div>
+          </div>
+          <div className="page_footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -4,48 +4,44 @@ import ProjectContainer from "./ProjectContainer";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-
 const ProjectPage = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
-	return (
-		<>
-			<div className="page_content">
-				<Navbar active="projects" />
-				<div className="content_wrapper">
-					<div className="projects_container">
-                        <div className=" title projects_title">
-                            These are some Projects I can do.
-                        </div>
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <div className="page_content">
+        <Navbar active="projects" />
+        <div className="content_wrapper">
+          <div className="projects_container">
+            <div className=" title projects_title">
+              These are some Projects I can do.
+            </div>
 
-                        <div className="subtitle projects_sub">
-                            I've completed some projects and coding exercises during my bootcamp
-                            and I'm proud of the progress I've made. Many of
-                            these projects are open-source and available for
-                            others to explore and contribute to. If you're
-                            interested in any of the projects I've worked on,
-                            please feel free to check out the code and suggest
-                            any improvements or enhancements you might have in
-                            mind. Collaborating with others is a great way to
-                            learn and grow, and I'm always open to new ideas and
-                            feedback.
-                        </div>
-                    </div>
-                    <hr/>
+            <div className="subtitle projects_sub">
+              I've completed some projects and coding exercises during my
+              journey in learning software development and I'm proud of the
+              progress I've made. Many of these projects are open-source and
+              available for others to explore and contribute to. If you're
+              interested in any of the projects I've worked on, please feel free
+              to check out the code and suggest any improvements or enhancements
+              you might have in mind. Collaborating with others is a great way
+              to learn and grow, and I'm always open to new ideas and feedback.
+            </div>
+          </div>
+          <hr />
 
-                    <div className="projects_list">
-                        <ProjectContainer />
-                    </div>
-                    {/* </div> */}
-                    <div className="page_footer">
-                        <Footer />
-                    </div>
-					
-				</div>
-			</div>
-		</>
-	);
+          <div className="projects_list">
+            <ProjectContainer />
+          </div>
+          {/* </div> */}
+          <div className="page_footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectPage;
